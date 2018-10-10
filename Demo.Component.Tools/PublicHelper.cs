59f2587170ef8c2ba48ede6478d6d1684c873e6d
tonybuff <471447434@qq.com>
2018-10-10 10:38:24 +0800

    添加一些公共的方法

--- Demo.Component.Tools/PublicHelper.cs
+++ Demo.Component.Tools/PublicHelper.cs
@@ -164,7 +164,30 @@ namespace Demo.Component.Tools
             HttpContext.Current.Request.Cookies.Remove(strName);
         }
 
- 
+        //public static string Hash(string password, string salt)
+        //{
+        //    // derive a 256-bit subkey (use HMACSHA1 with 10,000 iterations)
+        //    string hashed = Convert.ToBase64String(KeyDerivation.Pbkdf2(
+        //        password: password,
+        //        salt: Convert.FromBase64String(salt),
+        //        prf: KeyDerivationPrf.HMACSHA1,
+        //        iterationCount: 10000,
+        //        numBytesRequested: 256 / 8));
+
+        //    return hashed;
+        //}
+
+        public static string GetSalt()
+        {
+            // generate a 128-bit salt using a secure PRNG
+            byte[] salt = new byte[128 / 8];
+            using (var rng = RandomNumberGenerator.Create())
+            {
+                rng.GetBytes(salt);
+            }
+
+            return Convert.ToBase64String(salt);
+        }
 
 
     }
